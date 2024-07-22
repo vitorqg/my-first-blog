cf563e6eda10029c6f83b6e134c27bbcc116b23b
vitorqg <vaquiroagarcia2006@elvallecolegio.edu.gt>
2024-07-22 13:48:02 -0600

    Initial commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,9 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     return render(request, 'blog/post_list.html', {"posts":posts})
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+def contact(request):
+    return render(request, 'blog/contact.html')
